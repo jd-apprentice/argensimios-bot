8b5674a4496f7923fca9059e0bc09d9d9cf9f45a
jd-apprentice <emeraldusk@gmail.com>
2022-04-14 20:38:12 -0300

    first phase bot

---
+++ src/commands/infobae.ts
@@ -0,0 +1,25 @@
+import type { CommandInteraction } from "discord.js";
+import type { SimpleCommandMessage } from "discordx";
+import { Discord, SimpleCommand, Slash } from "discordx";
+import fetch from "node-fetch";
+
+@Discord()
+export class Notice {
+  @Slash("infobae")
+  private async notices(interaction: CommandInteraction): Promise<void> {
+    const response = await fetch(
+      "https://infobae-api.herokuapp.com/api/infobae/economia"
+    );
+    type Response = {
+      lastmod: string;
+      link: string;
+    };
+    const json: Response = await response.json();
+    await interaction.reply(json.link);
+  }
+
+  @SimpleCommand("infobae")
+  simpleNotice(command: SimpleCommandMessage): void {
+    this.notices(command.message);
+  }
+}
